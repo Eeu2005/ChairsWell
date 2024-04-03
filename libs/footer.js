f48108134b3e897eb69a5779d72470f92b6bc3cd
Eeu2005 <135387034+Eeu2005@users.noreply.github.com>
2024-04-03 17:40:37 -0300

    agora sim

--- libs/footer.js
+++ libs/footer.js
@@ -7,10 +7,19 @@ p.className = "meunome";
 p.innerText = "A.K.A Emanuel";
 pNome.addEventListener( "mouseover", ( event ) =>
 {
+    p.style.display="block"
+    console.log( event );
     p.style.position = "Absolute";
-    p.style.top = String( event.y );
-    p.style.left = String( event.x );
+    p.style.top = String( event.pageY ) + "px";
+    p.style.left = String( event.x ) + "px";
     console.log( p.style.top );
     document.querySelector( "body" ).appendChild( p );
+}
+);
+pNome.addEventListener( "mouseout", ( event ) =>
+{
+    console.log( event );
+    p.style.display = "none";
+   
 }
 );
